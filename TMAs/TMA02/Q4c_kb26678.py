f7ab6295ba510e07771b62443077354d8e6aee05
MeltMyGpu <102612447+MeltMyGpu@users.noreply.github.com>
2023-07-02 12:07:22 +0100

    Finsihed TMA files

--- TMAs/TMA02/Q4c_kb26678.py
+++ TMAs/TMA02/Q4c_kb26678.py
@@ -2,12 +2,12 @@
 Kieran Burnett - 25/06/23 - TMA02 Q4c Part III
 '''
 
-def calculate_painting_pricing_estimation(surface, price_per_gallon) -> float:
+def calculate_painting_pricing_estimation(surface, price_per_gallon):
     ''' 
     Calculates the estimated cost of a painting job using internal CONST values.
         INPUT: surface -> (non-zero, non-negative, integer) The surface area in metres squared of the wall that is to be painted.
         INPUT: price_per_gallon -> (non-zero, non-negative, float, rounded to 2 decimal places) The cost in pounds sterling per gallon of paint.
-        OUTPUT: estimated_cost -> (non-zero, non-neagtive, float, rounded to 2 decimal places) The estimated cost of the painting job.
+        OUTPUT: estimated_cost -> (non-zero, non-negative, float, rounded to 2 decimal places) The estimated cost of the painting job.
     '''
     # Init const values
     METER_PER_GALL = 60
@@ -31,4 +31,4 @@ def calculate_painting_pricing_estimation(surface, price_per_gallon) -> float:
 
 result = calculate_painting_pricing_estimation(360,14.00)
 print(result)
-assert result == 1077.51
+assert result == 1077.53
